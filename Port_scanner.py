2a22ee7a3a04c75ceebfb6f5bc729e07fce8969a
Avernell Christopher-Melikhov <94215685+AviChristopher@users.noreply.github.com>
2026-06-22 10:12:04 -0400

    Add files via upload
    This application takes a target IP address and scans a range of TCP ports to identify which ports are open using Python 's socket module

---
+++ Port_scanner.py
@@ -0,0 +1,20 @@
+import socket    # networking tool
+
+t = "Add target or Ip Address" # Add target/ IP Address
+
+print(f"Scanning{t}....\n")
+
+for port in range (1, 65535):   #Testing ports one by one
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a connection tool in the module socket
+    socket.setdefaulttimeout(1)
+
+    result= s.connect_ex((t, port )) # method tries to connect to the IP and ports
+
+    if result == 0:    #Then prints the port thats open
+        print(f"Port {port} is open")
+
+    s.close()
+
+
+
+
